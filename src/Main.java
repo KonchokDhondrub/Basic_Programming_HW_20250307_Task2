7cf469d459e6ff679761a42a0ae92a1799da640e
kd <erm.melnik@gmail.com>
2025-03-09 20:05:24 +0100

    Initial Commit

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+public class Main {
+    public static void main(String[] args) {
+        Programmer programmer = new Programmer("Иван", "занят");
+
+        try {
+            String result = programmer.doWork("Написать код");
+            System.out.println("Результат: " + result);
+        } catch (ProgrammerBusyException e) {
+            System.out.println(e.getMessage());
+        } catch (ProgrammerUnavailableNowException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
+
+class ProgrammerBusyException extends Exception {
+    public ProgrammerBusyException(String message) {
+        super(message);
+    }
+}
+
+class ProgrammerUnavailableNowException extends Exception {
+    public ProgrammerUnavailableNowException(String message) {
+        super(message);
+    }
+}
---
+++ src/Programmer.java
@@ -0,0 +1,22 @@
+class Programmer {
+    private String name;
+    private String status;
+
+    public Programmer(String name, String status) {
+        this.name = name;
+        this.status = status;
+    }
+
+    public String doWork(String taskMsg) throws ProgrammerBusyException, ProgrammerUnavailableNowException {
+        switch (status) {
+            case "доступен":
+                return taskMsg.toUpperCase();
+            case "занят":
+                throw new ProgrammerBusyException("Программист занят другой задачей");
+            case "не доступен":
+                throw new ProgrammerUnavailableNowException("Программист не доступен");
+            default:
+                throw new IllegalStateException("Некорректный статус программиста");
+        }
+    }
+}
